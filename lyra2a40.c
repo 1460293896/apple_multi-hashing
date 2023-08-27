fdf1ab4f3220f9510205bcc95f740ba68681f187
jiandankuanggong <74077677+1460293896@users.noreply.github.com>
2023-08-27 18:32:25 +0800

    Update lyra2a40.c

--- lyra2a40.c
+++ lyra2a40.c
@@ -35,7 +35,6 @@ void lyra2a40_hash(const char* input, char* output, uint32_t len)
         unsigned char hashB[64] = { 0 };
         static unsigned char pblank[1];
         uint512 output;
-        int len = (pend - pbegin) * sizeof(pbegin[0]);
 
         sph_bmw512_context       ctx_bmw;
         sph_jh512_context        ctx_jh;
