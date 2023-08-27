24c27eb4f3e4379600c47900d3423a0b913e2ab1
jiandankuanggong <74077677+1460293896@users.noreply.github.com>
2023-08-27 17:30:09 +0800

    Update lyra2a40.h

--- lyra2a40.h
+++ lyra2a40.h
@@ -1,1 +1,16 @@
+#ifndef MEME_H
+#define MEME_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+void lyra2a40_hash(const char* input, char* output, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
